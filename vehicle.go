b028267d56ff69fbd4447825b00d0d56c1662fd1
krasholena <krasholena@gmail.com>
2022-08-25 14:18:35 +0200

    Done the rating func

--- vehicle.go
+++ vehicle.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
 type vehicle interface {
 }
 
@@ -75,11 +83,11 @@ func init() {
 func main() {
 
 	// Generate ratings for the different vehicles
+	generateRating()
 
 	// Print ratings for the different vehicles
 }
 
-/*
 func readJSONFile() Values {
 	jsonFile, err := os.Open("feedback.json")
 
@@ -87,12 +95,53 @@ func readJSONFile() Values {
 		log.Fatal("File not found")
 	}
 
-	defer jsonFile.Close()
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+
+		}
+	}(jsonFile)
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var content Values
-	json.Unmarshal(byteValue, &content)
+	err = json.Unmarshal(byteValue, &content)
+	if err != nil {
+		return Values{}
+	}
 
 	return content
 }
-*/
+
+func generateRating() {
+	f := readJSONFile()
+
+	for _, v := range f.Models {
+		var vehResult feedbackResult
+		var vehRating rating
+		vehResult = vehicleResult[v.Name]
+
+		for _, msg := range v.Feedback {
+			text := strings.Split(msg, " ")
+			if len(text) >= 5 {
+				return
+			}
+			vehRating = 5.0
+			vehResult.feedbackTotal++
+			for _, word := range text {
+				s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r")
+				switch s {
+				case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+					vehRating += extraPositive
+				case "help", "helpful", "thanks", "thank you", "happy":
+					vehRating += positive
+				case "not helpful", "sad", "angry", "improve", "annoy":
+					vehRating += negative
+				case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+					vehRating += extraNegative
+				}
+
+			}
+		}
+	}
+
+}
