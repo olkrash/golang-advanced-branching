e044b81ed67fd1130e457bf244fd50718a580073
krasholena <krasholena@gmail.com>
2022-08-29 09:24:42 +0200

    Refactoring

--- vehicle.go
+++ vehicle.go
@@ -118,28 +118,25 @@ func generateRating() {
 	for _, v := range f.Models {
 		var vehResult feedbackResult
 		var vehRating rating
-		vehResult = vehicleResult[v.Name]
+		//vehResult = vehicleResult[v.Name]
 
 		for _, msg := range v.Feedback {
-			text := strings.Split(msg, " ")
-			if len(text) >= 5 {
-				return
-			}
-			vehRating = 5.0
-			vehResult.feedbackTotal++
-			for _, word := range text {
-				s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r")
-				switch s {
-				case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
-					vehRating += extraPositive
-				case "help", "helpful", "thanks", "thank you", "happy":
-					vehRating += positive
-				case "not helpful", "sad", "angry", "improve", "annoy":
-					vehRating += negative
-				case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
-					vehRating += extraNegative
+			if text := strings.Split(msg, " "); len(text) >= 5 {
+				vehRating = 5.0
+				vehResult.feedbackTotal++
+				for _, word := range text {
+					s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r")
+					switch s {
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
+					}
 				}
-
 			}
 		}
 	}
